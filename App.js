c6fc1fcb663536262f2b9e8031eb147ade97be67
kf <7kfpun@gmail.com>
2017-06-22 21:43:49 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity,
+} from 'react-native';
+
+import SafariView from 'react-native-safari-view';
+import SearchBar from 'react-native-material-design-searchbar';
+
+import fuzzy from 'fuzzy';
+
+import data from './data';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  row: {
+    flex: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  nameText: {
+    fontSize: 14,
+  },
+  descriptionText: {
+    fontSize: 14,
+    fontWeight: '300',
+    lineHeight: 36,
+  },
+});
+
+
+export default class Blog extends Component {
+  state = {
+    data,
+    matches: data,
+  }
+
+  onSearch(text) {
+    console.log('onSearch', text);
+    if (!text || typeof text !== 'string') {
+      this.setState({ matches: this.state.data });
+    } else {
+      const options = {
+        extract: item => item.key,
+      };
+      const results = fuzzy.filter(text, this.state.data, options);
+      this.setState({ matches: results.map(item => item.original) });
+    }
+  }
+
+  openUrl(url) {
+    SafariView.show({ url });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ marginTop: 20 }}>
+          <SearchBar
+            onSearchChange={item => this.onSearch(item)}
+            height={50}
+            onFocus={() => console.log('On Focus')}
+            onBlur={() => console.log('On Blur')}
+            placeholder={'Search...'}
+            autoCorrect={false}
+            // padding={5}
+            returnKeyType={'search'}
+          />
+        </View>
+        <FlatList
+          data={this.state.matches}
+          renderItem={({ item }) => (<TouchableOpacity onPress={() => this.openUrl(item.url)}>
+            <View style={styles.row}>
+              <Text style={styles.nameText}>{item.key}</Text>
+              <Text style={styles.descriptionText}>{item.description}</Text>
+            </View>
+          </TouchableOpacity>)}
+        />
+      </View>
+    );
+  }
+}
---
+++ data.js
@@ -0,0 +1,615 @@
+const data = [{
+  key: '500px',
+  url: 'https://developers.500px.com/',
+  description: '500px Engineering Blog',
+}, {
+  key: '99Designs',
+  url: 'https://99designs.com/tech-blog/',
+  description: '99Designs Tech Blog',
+}, {
+  key: '9GAG',
+  url: 'https://engineering.9gag.com/',
+  description: '9GAG Engineering Blog',
+}, {
+  key: 'Advanced Web Machinery',
+  url: 'https://advancedweb.hu/',
+  description: 'Advanced web Machinery Blog',
+}, {
+  key: 'AirBnB',
+  url: 'http://nerds.airbnb.com/',
+  description: 'AirBnB Engineering blog',
+}, {
+  key: 'AirBrake',
+  url: 'https://airbrake.io/blog/',
+  description: 'AirBrake Blog',
+}, {
+  key: 'Amazon',
+  url: 'https://developer.amazon.com/blogs',
+  description: 'Amazon Developer Blog',
+}, {
+  key: 'Artsy',
+  url: 'http://artsy.github.io/',
+  description: 'Artsy Engineering Blog',
+}, {
+  key: 'Asana',
+  url: 'https://blog.asana.com/category/eng/',
+  description: 'Asana Engineering',
+}, {
+  key: 'Atlassian',
+  url: 'https://developer.atlassian.com/blog/',
+  description: 'Atlassian Engineering Blog',
+}, {
+  key: 'Avant',
+  url: 'http://avant.engineering/',
+  description: 'Avant Engineering',
+}, {
+  key: 'Backtrace',
+  url: 'https://backtrace.io/blog/',
+  description: 'Backtrace Blog',
+}, {
+  key: 'BandCamp',
+  url: 'https://bandcamptech.wordpress.com/',
+  description: 'Bandcamp Tech',
+}, {
+  key: 'BankSimple',
+  url: 'https://www.simple.com/engineering',
+  description: 'Simple Blog',
+}, {
+  key: 'BenefitFocus',
+  url: 'https://www.benefitfocus.com/blogs/design-engineering',
+  description: 'Benefitfocus Engineering Blog',
+}, {
+  key: 'BetterWorks',
+  url: 'https://engineering.betterworks.com/',
+  description: 'Betterworks Blog',
+}, {
+  key: 'Bitly',
+  url: 'http://word.bitly.com/',
+  description: 'Bitly Engineering',
+}, {
+  key: 'BitTorrent',
+  url: 'http://engineering.bittorrent.com/',
+  description: 'BitTorrent Engineering Blog',
+}, {
+  key: 'Blablacar',
+  url: 'http://blablatech.com/',
+  description: 'Blablacar Engineering Blog',
+}, {
+  key: 'BlackRock Engineering',
+  url: 'http://rockthecode.io/',
+  description: 'BlackRock Engineering Blog',
+}, {
+  key: 'Branch',
+  url: 'https://blog.branch.io/',
+  description: 'Branch Blog',
+}, {
+  key: 'Booking',
+  url: 'https://blog.booking.com/',
+  description: 'Booking.com Blog',
+}, {
+  key: 'BookMyShow',
+  url: 'https://we-are.bookmyshow.com/',
+  description: 'BookMyShow Blog',
+}, {
+  key: 'Canva',
+  url: 'https://engineering.canva.com/',
+  description: 'Canva Blog',
+}, {
+  key: 'Captricity',
+  url: 'http://captricity.com/blog/',
+  description: 'Captricity Blog',
+}, {
+  key: 'Carbon Five',
+  url: 'http://blog.carbonfive.com/',
+  description: 'Carbon Five Blog',
+}, {
+  key: 'Cerner',
+  url: 'http://engineering.cerner.com/',
+  description: 'Engineering Health, A Cerner Blog',
+}, {
+  key: 'Chartbeat',
+  url: 'http://engineering.chartbeat.com/',
+  description: 'Chartbeat Engineering Blog',
+}, {
+  key: 'Cleartrip',
+  url: 'https://medium.com/cleartrip',
+  description: 'Cleartrip Blog',
+}, {
+  key: 'Close.io',
+  url: 'https://hack.close.io/',
+  description: 'Close.io Engineering blog',
+}, {
+  key: 'Cloudera',
+  url: 'http://blog.cloudera.com/blog/',
+  description: 'Cloudera Technical Blog',
+}, {
+  key: 'Cloudflare',
+  url: 'https://blog.cloudflare.com/',
+  description: 'Cloudflare blog',
+}, {
+  key: 'Cloudscaling',
+  url: 'http://cloudscaling.com/blog/',
+  description: 'Cloudscaling Blog',
+}, {
+  key: 'Compass',
+  url: 'https://medium.com/compass-true-north/',
+  description: 'Compass Engineering Blog',
+}, {
+  key: 'cookpad',
+  url: 'https://sourcediving.com/',
+  description: 'cookpad Blog',
+}, {
+  key: 'Confluent',
+  url: 'http://www.confluent.io/blog',
+  description: 'Confluent Blog',
+}, {
+  key: 'Codeminer42',
+  url: 'https://blog.codeminer42.com/',
+  description: 'The Miners, Codeminer42 Engineering Blog',
+}, {
+  key: 'Coursera',
+  url: 'https://blog.coursera.org/',
+  description: 'Coursera Blog',
+}, {
+  key: 'ClusterHQ',
+  url: 'https://clusterhq.com/blog/',
+  description: 'ClusterHQ Blog',
+}, {
+  key: 'Crowdbooster',
+  url: 'http://blog.crowdbooster.com/',
+  description: 'Crowdbooster Blog',
+}, {
+  key: 'Dailymotion',
+  url: 'http://engineering.dailymotion.com/',
+  description: 'Dailymotion Engineering Blog',
+}, {
+  key: 'Datafloq',
+  url: 'https://datafloq.com/read/',
+  description: 'Datafloq Blog',
+}, {
+  key: 'Dataiku',
+  url: 'https://www.dataiku.com/blog/',
+  description: 'Dataiku Blog',
+}, {
+  key: 'DCHQ.io',
+  url: 'http://dchq.co/blog.html',
+  description: 'DCHQ Blog',
+}, {
+  key: 'Making Dia',
+  url: 'https://making.dia.com/',
+  description: 'Dia&Co. Engineering Blog',
+}, {
+  key: 'Docker Blog',
+  url: 'https://blog.docker.com/',
+  description: 'Docker Blog',
+}, {
+  key: 'Dropbox',
+  url: 'https://blogs.dropbox.com/tech/',
+  description: 'Dropbox Tech Blog',
+}, {
+  key: 'ebay',
+  url: 'http://www.ebaytechblog.com',
+  description: 'eBay Tech Blog',
+}, {
+  key: 'Elastic',
+  url: 'https://www.elastic.co/blog',
+  description: 'Elastic Blog',
+}, {
+  key: 'EngineYard',
+  url: 'https://blog.engineyard.com/',
+  description: 'Engine Yard Blog',
+}, {
+  key: 'Etsy',
+  url: 'https://codeascraft.com/',
+  description: 'Code As Craft, Etsy Engineering Blog',
+}, {
+  key: 'Eventbrite',
+  url: 'https://www.eventbrite.com/engineering/',
+  description: 'Eventbrite Engineering Blog',
+}, {
+  key: 'Evernote',
+  url: 'https://blog.evernote.com/tech/',
+  description: 'Evernote Tech Blog | The Care and Feeding of Elephants',
+}, {
+  key: 'Facebook',
+  url: 'https://code.facebook.com/posts/',
+  description: 'Facebook Engineering Blog',
+}, {
+  key: 'FastMail',
+  url: 'https://blog.fastmail.com/',
+  description: 'FastMail Blog',
+}, {
+  key: 'FiftyThree',
+  url: 'http://making.fiftythree.com/',
+  description: 'Making Fifty Three',
+}, {
+  key: 'Flickr',
+  url: 'http://code.flickr.net/',
+  description: 'Code.flickr.com',
+}, {
+  key: 'Flipboard',
+  url: 'http://engineering.flipboard.com/',
+  description: 'Flipboard Engineering',
+}, {
+  key: 'Flipkart',
+  url: 'http://tech-blog.flipkart.net/',
+  description: 'Flipkart Engineering',
+}, {
+  key: 'Fluidinfo',
+  url: 'http://blogs.fluidinfo.com/',
+  description: 'Fluidinfo Blog',
+}, {
+  key: 'FourSquare',
+  url: 'http://engineering.foursquare.com/',
+  description: 'Foursquare Engineering Blog',
+}, {
+  key: 'GitHub',
+  url: 'http://githubengineering.com/',
+  description: 'The GitHub Blog: Engineering',
+}, {
+  key: 'Gnip',
+  url: 'https://engineering.gnip.com/',
+  description: 'Gnip Engineering',
+}, {
+  key: 'Google Developers',
+  url: 'https://developers.googleblog.com/',
+  description: 'Google Developers Blog',
+}, {
+  key: 'GoSquared',
+  url: 'https://engineering.gosquared.com/',
+  description: 'GoSquared Engineering Blog',
+}, {
+  key: 'Grouper',
+  url: 'http://eng.joingrouper.com/',
+  description: 'Grouper Engineering Team Blog',
+}, {
+  key: 'Groupon',
+  url: 'https://engineering.groupon.com/',
+  description: 'Groupon Engineering Blog',
+}, {
+  key: 'Harry\'s',
+  url: 'http://engineering.harrys.com/',
+  description: 'Harry\'s Engineering',
+}, {
+  key: 'Heroku',
+  url: 'https://engineering.heroku.com/',
+  description: 'Heroku Engineering Blog',
+}, {
+  key: 'HoneyBadger',
+  url: 'http://blog.honeybadger.io',
+  description: 'HoneyBadger Blog',
+}, {
+  key: 'Housing',
+  url: 'https://medium.com/engineering-housing',
+  description: 'Housing Engineering Blog',
+}, {
+  key: 'HubbleHQ',
+  url: 'https://hubblehq.com/blog',
+  description: 'HubbleHQ Blog',
+}, {
+  key: 'Icehouse',
+  url: 'https://www.icehousecorp.com/blog/',
+  description: 'Ice House Blog',
+}, {
+  key: 'Import.io',
+  url: 'https://www.import.io/blog/',
+  description: 'Import.io Blog',
+}, {
+  key: 'Indeed',
+  url: 'http://engineering.indeedblog.com/blog/',
+  description: 'Indeed Engineering',
+}, {
+  key: 'Influxdata',
+  url: 'https://influxdata.com/blog/',
+  description: 'Influxdata Blog',
+}, {
+  key: 'Instacart',
+  url: 'https://tech.instacart.com/',
+  description: 'Instacart Tech Blog',
+}, {
+  key: 'Instagram',
+  url: 'http://instagram-engineering.tumblr.com/',
+  description: 'Instagram Engineering',
+}, {
+  key: 'IntentHQ',
+  url: 'http://engineering.intenthq.com/',
+  description: 'IntentHQ Engineering',
+}, {
+  key: 'Intercom.io',
+  url: 'https://blog.intercom.io',
+  description: 'Intercom Blog',
+}, {
+  key: 'Jampp',
+  url: 'http://geeks.jampp.com/',
+  description: 'Jampp\'s Engineering Blog',
+}, {
+  key: 'Jelastic',
+  url: 'http://blog.jelastic.com/',
+  description: 'Jelastic Blog',
+}, {
+  key: 'Jolly Good Code',
+  url: 'https://jollygoodcode.github.io/',
+  description: 'Jolly Good Code Blog',
+}, {
+  key: 'Kaggle',
+  url: 'http://blog.kaggle.com/',
+  description: 'Kaggle Blog',
+}, {
+  key: 'Khan Academy Engineering Blog',
+  url: 'http://engineering.khanacademy.org/',
+  description: 'Khan Academy Engineering Blog',
+}, {
+  key: 'Levvel',
+  url: 'https://www.levvel.io/blog/',
+  description: 'Levvel Blog',
+}, {
+  key: 'LinkedIn',
+  url: 'https://engineering.linkedin.com/blog',
+  description: 'LinkedIn Engineering',
+}, {
+  key: 'LiveChat',
+  url: 'https://developers.livechatinc.com/blog/',
+  description: 'LiveChat Developers Blog',
+}, {
+  key: 'Machinalis',
+  url: 'http://www.machinalis.com/blog/',
+  description: 'Machinalis Blog',
+}, {
+  key: 'Mandrill',
+  url: 'http://blog.mandrill.com/',
+  description: 'Mandrill Blog',
+}, {
+  key: 'Medallia',
+  url: 'http://engineering.medallia.com/blog/',
+  description: 'Medallia Engineering Blog',
+}, {
+  key: 'Mixpanel',
+  url: 'https://mixpanel.com/blog/',
+  description: 'Mixpanel Blog',
+}, {
+  key: 'Monetate',
+  url: 'http://engineering.monetate.com/',
+  description: 'Monetate Engineering Blog',
+}, {
+  key: 'Monsanto',
+  url: 'http://engineering.monsanto.com/',
+  description: 'Monsanto Engineering Blog',
+}, {
+  key: 'Myntra',
+  url: 'https://medium.com/myntra-engineering',
+  description: 'Myntra Engineering Blog',
+}, {
+  key: 'Netflix',
+  url: 'http://techblog.netflix.com/',
+  description: 'Netflix Tech Blog',
+}, {
+  key: 'Oyster',
+  url: 'http://tech.oyster.com/',
+  description: 'Insights from Oyster.com\'s Tech Team',
+}, {
+  key: 'Pagerduty',
+  url: 'https://www.pagerduty.com/blog/',
+  description: 'Pagerduty Blog',
+}, {
+  key: 'Pagico',
+  url: 'https://www.pagico.com/blog/',
+  description: 'Pagico Blog',
+}, {
+  key: 'PayPal',
+  url: 'https://www.paypal-engineering.com/',
+  description: 'PayPal Engineering',
+}, {
+  key: 'Persiscope Data',
+  url: 'https://www.periscopedata.com/blog',
+  description: 'Periscope Data Blog',
+}, {
+  key: 'Phyramid',
+  url: 'https://www.phyramid.com/blog/',
+  description: 'Phyramid Blog',
+}, {
+  key: 'Pinterest',
+  url: 'https://engineering.pinterest.com/',
+  description: 'Making Pinterest',
+}, {
+  key: 'Postmarkapp',
+  url: 'https://postmarkapp.com/blog',
+  description: 'Postmarkapp Blog',
+}, {
+  key: 'Practo',
+  url: 'https://medium.com/practo-engineering',
+  description: 'Practo Engineering Blog',
+}, {
+  key: 'Prezi',
+  url: 'http://engineering.prezi.com/',
+  description: 'Prezi Engineering',
+}, {
+  key: 'Quora',
+  url: 'http://engineering.quora.com/',
+  description: 'Engineering at Quora',
+}, {
+  key: 'Raintank',
+  url: 'https://blog.raintank.io/',
+  description: 'Raintank Blog',
+}, {
+  key: 'Algorithm & Blues',
+  url: 'https://algorithms.rdio.com/',
+  description: 'Rdio Engineering blog',
+}, {
+  key: 'REA Group',
+  url: 'http://techblog.realestate.com.au/engineering/',
+  description: 'REA Group Engineering Blo',
+}, {
+  key: 'Reddit',
+  url: 'http://www.redditblog.com/',
+  description: 'What\'s New on Reddit',
+}, {
+  key: 'Rightscale',
+  url: 'http://eng.rightscale.com/',
+  description: 'Rightscale Engineering Blog',
+}, {
+  key: 'Riot Games Engineering',
+  url: 'http://engineering.riotgames.com/',
+  description: 'Riot Games Engineering Blog',
+}, {
+  key: 'Salesforce',
+  url: 'https://developer.salesforce.com/blogs/engineering/',
+  description: 'Salesforce Engineering Blog',
+}, {
+  key: 'Sensible',
+  url: 'http://blog.sensible.io/',
+  description: 'Sensible Blog',
+}, {
+  key: 'Shopify',
+  url: 'https://engineering.shopify.com',
+  description: 'Shopify Engineering Blog',
+}, {
+  key: 'Simulmedia',
+  url: 'http://www.simulmedia.com/resources/blog/categories/engineering/',
+  description: 'Simulmedia Engineering',
+}, {
+  key: 'Slack',
+  url: 'https://slack.engineering/',
+  description: 'Slack Engineering Blog',
+}, {
+  key: 'SlideShare',
+  url: 'http://engineering.slideshare.net/',
+  description: 'SlideShare Engineering Blog',
+}, {
+  key: 'SmugMug',
+  url: 'http://engineering.slideshare.com/',
+  description: 'SmugMug\'s Don MacAskill',
+}, {
+  key: 'Songkick',
+  url: 'http://devblog.songkick.com/',
+  description: 'Songkick Technology Blog',
+}, {
+  key: 'SoundCloud',
+  url: 'https://developers.soundcloud.com/blog/',
+  description: 'SoundCloud Backstage',
+}, {
+  key: 'Spotify',
+  url: 'https://labs.spotify.com/',
+  description: 'Spotify\'s Engineering and Technology Blog',
+}, {
+  key: 'Square',
+  url: 'https://corner.squareup.com/',
+  description: 'The Corner, Square Engineering Blog',
+}, {
+  key: 'Strava',
+  url: 'http://labs.strava.com',
+  description: 'Strava Labs',
+}, {
+  key: 'Stitch Fix',
+  url: 'http://multithreaded.stitchfix.com/blog/',
+  description: 'Stitch Fix Engineering Blog',
+}, {
+  key: 'StormPath',
+  url: 'https://stormpath.com/blog/',
+  description: 'StormPath Blog',
+}, {
+  key: 'Stylight',
+  url: 'http://tech.stylight.com/',
+  description: 'Stylight Tech Blo',
+}, {
+  key: 'TellApart',
+  url: 'https://www.tellapart.com/engineering-blog/',
+  description: 'TellApart Blog',
+}, {
+  key: 'ThoughtWorks',
+  url: 'https://www.thoughtworks.com/insights',
+  description: 'ThoughtWorks Engineering Blog',
+}, {
+  key: 'Tilt',
+  url: 'http://engineering.tilt.com/',
+  description: 'Tilt Engineering Blog',
+}, {
+  key: 'Toptal',
+  url: 'http://www.toptal.com/blog',
+  description: 'Toptal Blog',
+}, {
+  key: 'Tumblr',
+  url: 'http://engineering.tumblr.com/',
+  description: 'Tumblr Engineering',
+}, {
+  key: 'Twitter',
+  url: 'https://blog.twitter.com/engineering',
+  description: 'Twitter Engineering',
+}, {
+  key: 'Twilio',
+  url: 'https://www.twilio.com/engineering/',
+  description: 'Twilio Engineering',
+}, {
+  key: 'Thumbtack',
+  url: 'https://www.thumbtack.com/engineering/',
+  description: 'Thumbtack Engineering',
+}, {
+  key: 'Uber',
+  url: 'http://eng.uber.com/',
+  description: 'Uber Engineering',
+}, {
+  key: 'Udacity',
+  url: 'http://blog.udacity.com/',
+  description: 'Udacity Blog',
+}, {
+  key: 'Venmo',
+  url: 'http://blog.venmo.com/?category=Engineering',
+  description: 'Venmo Blog',
+}, {
+  key: 'Wayfair',
+  url: 'http://engineering.wayfair.com/',
+  description: 'Wayfair Engineering',
+}, {
+  key: 'Wealthfront',
+  url: 'http://eng.wealthfront.com/',
+  description: 'Wealthfront Engineering',
+}, {
+  key: 'WebEngage',
+  url: 'http://engineering.webengage.com/',
+  description: 'WebEngage Engineering Blog',
+}, {
+  key: 'Workshape',
+  url: 'http://blog.workshape.io/',
+  description: 'Workshape Blog',
+}, {
+  key: 'Yahoo!',
+  url: 'http://yahooeng.tumblr.com/',
+  description: 'Yahoo! Engineering Blog',
+}, {
+  key: 'Yammer',
+  url: 'https://medium.com/@YammerEng',
+  description: 'Blog Yammer Engineering',
+}, {
+  key: 'Yelp',
+  url: 'http://engineeringblog.yelp.com/',
+  description: 'Yelp Product and Engineering Blog',
+}, {
+  key: 'YouTube',
+  url: 'https://youtube-eng.googleblog.com/',
+  description: 'YouTube Engineering and Developers Blog',
+}, {
+  key: 'Zalando',
+  url: 'https://tech.zalando.com/blog/',
+  description: 'Zalando Tech Blog',
+}, {
+  key: 'Zapier',
+  url: 'https://zapier.com/engineering/',
+  description: 'Zapier Engineering Blog',
+}, {
+  key: 'Zenefits',
+  url: 'https://engineering.zenefits.com/posts/',
+  description: 'Zenefits Engineering Blog',
+}, {
+  key: 'ZenPayroll',
+  url: 'http://engineering.zenpayroll.com/',
+  description: 'ZenPayroll Engineering',
+}, {
+  key: 'Zillow',
+  url: 'https://engineering.zillow.com/',
+  description: 'Zillow Engineering Blog',
+}, {
+  key: 'Zomato',
+  url: 'https://engineering.zomato.com/',
+  description: 'Zomato Engineering Blog',
+}];
+
+export default data;
